42b6645ac9a82d1dfb298d7fcf66761ba6aa514f
Simon Beer <simonb33r@gmail.com>
2025-02-26 18:06:30 +0000

    Setup basic react login system (need to make it look good)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import Login from "./components/auth/login";
+import Register from "./components/auth/register";
+
+import Header from "./components/header";
+import Home from "./components/home";
+
+import { AuthProvider } from "./contexts/authContext";
+import { useRoutes } from "react-router-dom";
 
 function App() {
+  const routesArray = [
+    {
+      path: "*",
+      element: <Login />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+  ];
+  let routesElement = useRoutes(routesArray);
   return (
-    <div className="App">
-     <h1>hello world !  mzee here</h1>
-    </div>
+    <AuthProvider>
+      <Header />
+      <div className="w-full h-screen flex flex-col">{routesElement}</div>
+    </AuthProvider>
   );
 }
 
---
+++ src/firebase/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCgVyXi6_lJW-Q3TeDQYeCcHFuMy1-fN8",
+  authDomain: "pta-app-917f5.firebaseapp.com",
+  projectId: "pta-app-917f5",
+  storageBucket: "pta-app-917f5.firebasestorage.app",
+  messagingSenderId: "335430137054",
+  appId: "1:335430137054:web:7a9fb10266a59e1ca63e4f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+
+
+export { app, auth };
